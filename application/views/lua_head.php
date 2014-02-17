d76812b369d715be3d7dc91e478d1c60e7b96352
dev <dev@playcrab.com>
2014-02-17 23:59:39 +0800

    加入导航和分页

---
+++ application/views/lua_head.php
@@ -0,0 +1,18 @@
+<hr/>
+<div class="text-center">
+    <form class="form-inline">
+        <label class="text">
+            平台<input type="text" class="input-small" placeholder="平台">
+        </label>
+        <label class="text">
+            版本<input type="text" class="input-small" placeholder="版本号">
+        </label>
+        <label class="text">
+            PID<input type="text" class="input-small" placeholder="PID">
+        </label>
+        <label class="text">
+            UID<input type="text" class="input-small" placeholder="UID">
+        </label>
+        <button type="submit" class="btn">确定</button>
+    </form>
+</div>
--- application/views/lua_list.php
+++ application/views/lua_list.php
@@ -4,10 +4,40 @@
 	<meta charset="utf-8">
 	<title>Lua错误列表</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="/ci/asset/css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <link href="/ares/ci/asset/css/bootstrap.min.css" rel="stylesheet" media="screen">
 </head>
 <body>
-        <table border='1'>
+    <div class="navbar navbar-inverse navbar-fixed-top">
+        <div class="navbar-inner">
+            <a class="brand" href="#">错误报告系统</a>
+            <ul class="nav">
+                <li class="active"><a href="#">Lua错误页面</a></li>
+                <li><a href="#">统计页面</a></li>
+                <li class="dropdown">
+                    <a data-toggle="dropdown" class="dropdown-toggle" href="#">游戏选择<b class="caret"></b></a>
+                    <ul class="dropdown-menu">
+                        <li><a href=<?php echo $_SERVER['PHP_SELF']."?game=ares"?>>大掌门</a></li>
+                        <li><a href=<?php echo $_SERVER['PHP_SELF']."?game=hebe"?>>忍将</a></li>
+                        <li><a href=<?php echo $_SERVER['PHP_SELF']."?game=chaos"?>>乱世曲</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <br />
+    <br />
+    <br />
+
+
+<?php
+$game = $_REQUEST['game'];
+if(empty($game)){
+    $game = "ares";
+}
+?>
+    <h1><?php echo $game;?> - Lua错误详情</h1>
+<?php include "lua_head.php";?>
+        <table class="table table-bordered">
         <thead>
         <tr><th>ID</th><th>时间</th><th>平台</th><th>版本号</th><th>错误信息</th><th>UID</th><th>PID</th><th>分区</th></tr>
         <thead>
@@ -29,11 +59,19 @@
 ?>
         </tbody>
         </table>
+
+        <ul class="pager">
+            <li class="previous">
+            <a href="#">&larr;最新</a>
+            </li>
+            <li><a href="#">上一页</a></li>
+            <li><a href="#">下一页</a></li>
+            <li class="next">
+            <a href="#">最旧&rarr;</a>
+            </li>
+        </ul>
  
-
-
-    <h1> 错误记录表</h1>
     <script src="http://code.jquery.com/jquery.js"></script>
-    <script src="/ci/asset/js/bootstrap.min.js"></script>
+    <script src="/ares/ci/asset/js/bootstrap.min.js"></script>
 </body>
 </html>
